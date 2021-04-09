32c27d4f5ecd9835ba085f72a4a747aabf650130
andrew <umanking@gmail.com>
2021-04-09 15:18:55 +0900

    AppHeader 컴포넌트 props 전달 예제

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <app-header 
+    v-on:renew="renewStr"
+    v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
 
 export default {
-  name: 'App',
+  data: function(){
+    return {
+      str: 'Header.. from 상위컴포넌트'
+    }
+  },
   components: {
-    HelloWorld
+    'app-header': AppHeader
+  },
+  methods :{
+    renewStr: function(){
+      this.str = 'NEW Header';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
